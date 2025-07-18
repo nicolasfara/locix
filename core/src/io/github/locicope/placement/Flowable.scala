4a20558f33fd2b46682b7f3d3cd12d39eca05b93
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-07-18 15:00:01 +0200

    minor

--- core/src/io/github/locicope/placement/Flowable.scala
+++ core/src/io/github/locicope/placement/Flowable.scala
@@ -5,6 +5,6 @@ import io.github.locicope.network.Network
 import ox.flow.Flow
 
 trait Flowable[Flowing[_, _ <: Peer]]:
-  type Container[V]
+  type Container[_]
   def lift[V, P <: Peer](value: Container[V], isLocal: Boolean)(using Network): Flowing[V, P]
   def unlift[V, P <: Peer](value: Flowing[Flow[V], P])(using Network): Container[V]
