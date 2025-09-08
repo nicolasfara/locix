66345a967afd84d98463604ca3df5f63192b9c22
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-09-08 15:13:11 +0200

    feat: complete collective as effect

--- core/src/io/github/nicolasfara/locicope/PlacementType.scala
+++ core/src/io/github/nicolasfara/locicope/PlacementType.scala
@@ -1,9 +1,9 @@
 package io.github.nicolasfara.locicope
 
-import io.github.nicolasfara.locicope.Net.Net
+import io.github.nicolasfara.locicope.Net.{Net, getFlow, getValue}
 import io.github.nicolasfara.locicope.network.NetworkResource.ResourceReference
-import io.github.nicolasfara.locicope.placement.Peers.{ Peer, Quantifier, TiedToMultiple, TiedToSingle }
-import io.github.nicolasfara.locicope.serialization.Encoder
+import io.github.nicolasfara.locicope.placement.Peers.{Peer, Quantifier, TiedToMultiple, TiedToSingle}
+import io.github.nicolasfara.locicope.serialization.{Codec, Encoder}
 import ox.flow.Flow
 
 object PlacementType:
@@ -16,11 +16,32 @@ object PlacementType:
 
   trait PeerScope[P <: Peer]
 
-  def lift[V: Encoder, P <: Peer](value: Option[V], ref: ResourceReference): V on P = ???
-  def liftFlow[V: Encoder, P <: Peer](value: Option[Flow[V]], ref: ResourceReference): Flow[V] on P = ???
+  def lift[V: Encoder, P <: Peer](value: Option[V], ref: ResourceReference)(using net: Net): V on P =
+    value
+      .map: value =>
+        Net.setValue(value, ref)
+        Placement.Local(value, ref)
+      .getOrElse(Placement.Remote(ref))
 
-  extension [V, Remote <: Peer](p: on[V, Remote])
+  def liftFlow[V: Encoder, P <: Peer](value: Option[Flow[V]], ref: ResourceReference)(using net: Net): Flow[V] on P =
+    value
+      .map: flow =>
+        Net.setFlow(flow, ref)
+        Placement.Local(flow, ref)
+      .getOrElse(Placement.Remote(ref))
+      
+  extension [V: Codec, Remote <: Peer](p: Flow[V] on Remote)
+    def unwrap(using PeerScope[Remote], Net): Flow[V] = p match
+      case Placement.Local(value, _) => value
+      case Placement.LocalAll(value, ref) => throw IllegalStateException("Something went wrong, please report this issue.")
+      case Placement.Remote(ref) => getFlow(ref).fold(ex => throw IllegalStateException("Value not found", ex), identity)
+
+  extension [V: Codec, Remote <: Peer](p: on[V, Remote])
     def asFlow[Local <: { type Tie <: Quantifier[Remote] }](using PeerScope[Local], Net): Flow[V] = ???
-    def unwrap(using PeerScope[Remote], Net): V = ???
+    def unwrap(using PeerScope[Remote], Net): V = p match
+      case Placement.Local(value, _) => value
+      case Placement.LocalAll(value, ref) => throw IllegalStateException("Something went wrong, please report this issue.")
+      case Placement.Remote(ref) => getValue(ref).fold(ex => throw IllegalStateException("Value not found", ex), identity)
     def asLocal[Local <: TiedToSingle[Remote]](using PeerScope[Local], Net): V = ???
     def asLocalAll[Local <: TiedToMultiple[Remote]](using PeerScope[Local], Net): Map[Int, V] = ???
+end PlacementType
---
+++ core/test/src/io/github/nicolasfara/locicope/CollectiveTest.scala
@@ -0,0 +1,79 @@
+package io.github.nicolasfara.locicope
+
+import io.github.nicolasfara.locicope.Collective.{ collective, neighbors, repeat, Collective, OutboundMessage }
+import io.github.nicolasfara.locicope.Net.Net
+import io.github.nicolasfara.locicope.PlacementType.{ on, unwrap }
+import io.github.nicolasfara.locicope.network.NetworkResource.ResourceReference
+import io.github.nicolasfara.locicope.serialization.{ Decoder, Encoder }
+import io.github.nicolasfara.locicope.utils.CpsArch.Smartphone
+import org.scalamock.stubs.Stubs
+import org.scalatest.BeforeAndAfter
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import io.github.nicolasfara.locicope.utils.TestCodec.given
+import ox.flow.Flow
+
+import scala.concurrent.duration.DurationInt
+
+class CollectiveTest extends AnyFlatSpecLike, Matchers, Stubs, BeforeAndAfter:
+
+  private val netEffect = stub[Net.Effect]
+  given net: Locicope[Net.Effect](netEffect)
+
+  before:
+    resetStubs()
+
+  "A collective program" should "return an empty export when no spatial operator are used" in:
+    def temporalEvolution(using Net, Collective): Flow[Int] on Smartphone = collective(1.second):
+      repeat(0)(_ + 1)
+    // Setup stubs
+    var lastExport: OutboundMessage = null
+    (netEffect.getValues(_: ResourceReference)(using _: Decoder[OutboundMessage])).returns(_ => Right(Map.empty))
+    (netEffect.setValue(_: OutboundMessage, _: ResourceReference)(using _: Encoder[OutboundMessage])).returns(outbound => lastExport = outbound._1)
+    (netEffect.setFlow(_: Flow[Int], _: ResourceReference)(using _: Encoder[Int])).returns(_ => ())
+
+    Collective.run[Smartphone](using net):
+      val res = temporalEvolution(using net, summon)
+      res.unwrap.take(5).runToList() shouldBe List(1, 2, 3, 4, 5)
+
+    lastExport shouldBe Map.empty // No export since no spatial operator is used
+
+  it should "return an export with the value tree produced by spatial operators" in:
+    def spatialComputation(using Net, Collective): Flow[Int] on Smartphone = collective(1.second):
+      neighbors(1).local
+
+    // Setup stubs
+    var lastExport: OutboundMessage = null
+    (netEffect.getValues(_: ResourceReference)(using _: Decoder[OutboundMessage])).returns(_ => Right(Map.empty))
+    (netEffect.setValue(_: OutboundMessage, _: ResourceReference)(using _: Encoder[OutboundMessage])).returns(outbound => lastExport = outbound._1)
+    (netEffect.setFlow(_: Flow[Int], _: ResourceReference)(using _: Encoder[Int])).returns(_ => ())
+
+    Collective.run[Smartphone](using net):
+      val res = spatialComputation(using net, summon)
+      res.unwrap.take(5).runToList() shouldBe List(1, 1, 1, 1, 1) // Each round the value is the same since neighbors return the same values
+
+    val expectExport = Map("neighbors.0" -> summon[Encoder[Int]].encode(1))
+    lastExport.forall { case (key, bytes) => expectExport.get(key).exists(bytes.sameElements) } shouldBe true
+
+  it should "build a field with neighbors values aligned to the operator" in:
+    def spatialComputation(using Net, Collective): Flow[Int] on Smartphone = collective(1.second):
+      neighbors(1).sum
+
+    // Setup stubs
+    var lastExport: OutboundMessage = null
+    val neighborValues = Map(
+      1 -> Map("neighbors.0" -> summon[Encoder[Int]].encode(1)),
+      2 -> Map("neighbors.0" -> summon[Encoder[Int]].encode(2)),
+      3 -> Map("neighbors.0" -> summon[Encoder[Int]].encode(3)),
+    )
+    (netEffect.getValues(_: ResourceReference)(using _: Decoder[OutboundMessage])).returns(_ => Right(neighborValues))
+    (netEffect.setValue(_: OutboundMessage, _: ResourceReference)(using _: Encoder[OutboundMessage])).returns(outbound => lastExport = outbound._1)
+    (netEffect.setFlow(_: Flow[Int], _: ResourceReference)(using _: Encoder[Int])).returns(_ => ())
+
+    Collective.run[Smartphone](using net):
+      val res = spatialComputation(using net, summon)
+      res.unwrap.take(1).runToList() shouldBe List(7) // Summed values from neighbors (1 + 2 + 3 + local 1)
+
+    val expectExport = Map("neighbors.0" -> summon[Encoder[Int]].encode(1))
+    lastExport.forall { case (key, bytes) => expectExport.get(key).exists(bytes.sameElements) } shouldBe true
+end CollectiveTest
