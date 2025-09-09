1f47909dd59e97e21237e5b12f3c268445ad53d1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-09-09 10:01:20 +0200

    test: add branching test

--- core/test/src/io/github/nicolasfara/locicope/CollectiveTest.scala
+++ core/test/src/io/github/nicolasfara/locicope/CollectiveTest.scala
@@ -79,4 +79,27 @@ class CollectiveTest extends AnyFlatSpecLike, Matchers, Stubs, BeforeAndAfter:
 
     val expectExport = Map("neighbors.0" -> summon[Encoder[Int]].encode(1))
     lastExport.forall { case (key, bytes) => expectExport.get(key).exists(bytes.sameElements) } shouldBe true
+
+  it should "partition the network with device aligned to the branch condition" in:
+    def spatialComputation(using Net, Collective): Flow[Int] on Smartphone = collective(1.second):
+      Collective.branch(summon[Collective].effect.localId % 2 == 0) { neighbors(1).sum } { neighbors(2).sum }
+
+    // Setup stubs
+    var lastExport: OutboundMessage = null
+    val neighborValues = Map(
+      1 -> Map("branch[false].0/neighbors.0" -> summon[Encoder[Int]].encode(2)),
+      2 -> Map("branch[true].0/neighbors.0" -> summon[Encoder[Int]].encode(1)),
+      3 -> Map("branch[false].0/neighbors.0" -> summon[Encoder[Int]].encode(2)),
+    )
+    (() => netEffect.id).returnsWith(0) // Even id
+    (netEffect.getValues(_: ResourceReference)(using _: Decoder[OutboundMessage])).returnsWith(Right(neighborValues))
+    (netEffect.setValue(_: OutboundMessage, _: ResourceReference)(using _: Encoder[OutboundMessage])).returns(outbound => lastExport = outbound._1)
+    (netEffect.setFlow(_: Flow[Int], _: ResourceReference)(using _: Encoder[Int])).returnsWith(())
+
+    Collective.run[Smartphone](using net):
+      val res = spatialComputation(using net, summon)
+      res.unwrap.take(1).runToList() shouldBe List(2) // Each round the value is the same since branch return the same values
+
+    val expectExport = Map("branch[true].0/neighbors.0" -> summon[Encoder[Int]].encode(1))
+    lastExport.forall { case (key, bytes) => expectExport.get(key).exists(bytes.sameElements) } shouldBe true
 end CollectiveTest
