cfdf9b2942d3edee202a564463534e76b199c235
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-09-12 11:52:06 +0200

    setup test for multitier

--- core/test/src/io/github/nicolasfara/locicope/MultitierTest.scala
+++ core/test/src/io/github/nicolasfara/locicope/MultitierTest.scala
@@ -4,6 +4,11 @@ import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalamock.stubs.Stubs
 import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.should.Matchers
+import io.github.nicolasfara.locicope.Net.Net
+import io.github.nicolasfara.locicope.Multitier.Multitier
+import io.github.nicolasfara.locicope.utils.TestCodec.given
+import io.github.nicolasfara.locicope.Multitier.placed
+import io.github.nicolasfara.locicope.utils.ClientServerArch.{Client, Server }
 
 class MultitierTest extends AnyFlatSpecLike, Matchers, Stubs, BeforeAndAfter:
 
@@ -13,3 +18,12 @@ class MultitierTest extends AnyFlatSpecLike, Matchers, Stubs, BeforeAndAfter:
   before:
     resetStubs()
 
+  "The Multitier capability" should "access a remote value" in:
+    def multitierSingleValue(using Net, Multitier) = 
+      val valueOnServer = placed[Server](10)
+      placed[Client]:
+        val localValue = valueOnServer.asLocal
+        localValue shouldBe 10
+        localValue
+
+    Multitier.run[Client](multitierSingleValue)
