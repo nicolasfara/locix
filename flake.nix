35ca7e6360210925c3c16d779ff0c6858a2f04da
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-01-12 10:33:26 +0100

    build: add flake.nix deps

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# flake.nix
+{
+  description = "Dev shell for locix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells = {
+        "${system}" = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              openjdk17
+              sbt
+              git
+            ];
+
+            shellHook = ''
+              export JAVA_HOME=${pkgs.openjdk17}
+              export PATH=${pkgs.sbt}/bin:$PATH
+            '';
+          };
+        };
+      };
+    };
+}
